86cf545d23a0f2a0e31a9a3d2ba94a785f2958c4
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2022-03-25 13:10:52 -0300

    Implemented defender movement

--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -19,11 +20,33 @@
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  UNUSED(attacker_position);
+  UNUSED(defender_spy);
+  
+  static int generate = 0;
+  
+  if (generate == 0){
+    srand(getpid());
+    generate++;
+    }
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  int dir = rand() % 5;
+  
+  if (dir == 0){
+    return (direction_t) DIR_RIGHT;
+  }
+  else if (dir == 1){
+    return (direction_t) DIR_UP_RIGHT;
+  }
+  else if (dir == 2){
+    return (direction_t) DIR_DOWN_RIGHT;
+  }
+  else if (dir == 3){
+    return (direction_t) DIR_UP;
+  }
+  else if (dir == 4){
+    return (direction_t) DIR_DOWN;
+  }
 }
 
 /*----------------------------------------------------------------------------*/
